4587c7782fdf512a9b560b22ef89ee50c675cc79
Erick Veliz <erickveliz86@gmail.com>
2021-04-09 19:51:17 -0500

    primer commit

---
+++ SrChallenge.java
@@ -0,0 +1,6 @@
+website.sendText("myName", "name", "user-submitted-name", "Lucas Ocampo");
+website.sendText("myEmailAddress", "css", "user-submitted-email", "lucas@ocampo.com")
+website.sendText("myProductTitle", "css", "user-submitted-title", "Losoftware: Software Agil que ayuda al pago de tus servicios y envio de dinero al extranjero")
+website.sendText("myUrl", "xpath", "user-submitted-url", "http://losoftware.net/pagos")
+website.sendText("myContent", "css", "user-submitted-content", "software que te permite pagar tus servicios y enviar dinero al extranjero desde cualquier pais")
+website.sendText("myTags", "css", "user-submitted-tags", "software, pagar, servicios, ciudad")
